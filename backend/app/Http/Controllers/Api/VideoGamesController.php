d5d8370f496e2a4f0dfe94937e5826466097929e
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-20 17:00:34 +0100

    creato Api di index e show

---
+++ backend/app/Http/Controllers/Api/VideoGamesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\VideoGame;
+use Illuminate\Http\Request;
+
+class VideoGamesController extends Controller
+{
+    public function index()
+    {
+        $videoGames = VideoGame::with('consoles')->get();
+        return response()->json([
+            "success" => "true",
+            "data" => "videoGames"
+        ]);
+    }
+
+    public function show(VideoGame $videogame)
+    {
+        $videogame->load('consoles');
+
+        return response()->json([
+            "success" => "true",
+            "data" => $videogame
+        ]);
+    }
+}
