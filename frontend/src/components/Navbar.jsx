61cda30363bf8068604109932c225e9228b0c207
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-21 19:06:57 +0100

    add hook location

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import { Navbar, Container, Form, Button, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Form, Button, Nav } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 export default function CustomNavbar() {
+  const location = useLocation();
+
   return (
     <Navbar expand="lg" >
       <Container fluid >
-        <Navbar.Brand href="#" className='text-white border rounded p-2 fw-bold'>VideoGames</Navbar.Brand>
+        <Navbar.Brand className='text-white border rounded p-2 fw-bold'>VideoGames</Navbar.Brand>
         < Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto">
-            <Nav.Link href="#action1" className='text-white border btn btn-outline-secondary' disabled>Home</Nav.Link>
+            <Nav.Link href="/" className='text-white border btn btn-outline-dark' disabled={location.pathname === '/'} >Home</Nav.Link>
 
           </Nav>
           <Form className="d-flex">
